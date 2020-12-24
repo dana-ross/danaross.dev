208e216dec78501598a2210a3c25d4d5062b8e45
Dana Ross <dana@danaross.dev>
2020-12-24 12:58:08 -0500

    CSS processing

--- src/build.js
+++ src/build.js
@@ -2,10 +2,12 @@ const fs = require('fs')
 const processImages = require('./process-images')
 const processHTMLTemplates = require('./process-html-templates')
 const processScripts = require('./process-scripts')
+const processStylesheets = require('./process-stylesheets')
 
 const BUILD_DIR = './build/www'
 const PAGES_DIR = './pages/www'
 const SCRIPTS_DIR = './assets/scripts'
+const STYLES_DIR = './assets/stylesheets'
 
 // Re-create the build directory
 fs.rmdirSync(BUILD_DIR, { recursive: true })
@@ -14,3 +16,4 @@ fs.mkdirSync(BUILD_DIR, { recursive: true })
 processImages(BUILD_DIR)
 processHTMLTemplates(BUILD_DIR, PAGES_DIR)
 processScripts(BUILD_DIR, SCRIPTS_DIR)
+processStylesheets(BUILD_DIR, STYLES_DIR)
