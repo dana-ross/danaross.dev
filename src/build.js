df797080c5c6c1458a06a2f26bff435b5f563dfc
Dana Ross <dana@danaross.dev>
2020-12-24 11:51:52 -0500

    Blow away the 'build/www' directory

--- src/build.js
+++ src/build.js
@@ -1,11 +1,13 @@
+const fs = require('fs')
 const processImages = require('./process-images')
 const processHTMLTemplates = require('./process-html-templates')
 
 const BUILD_DIR = './build/www'
 const PAGES_DIR = './pages/www'
 
-fs.rmdir(BUILD_DIR, { recursive: true })
-fs.mkdirSync(BUILD_DIR, {recursive: true})
+// Re-create the build directory
+fs.rmdirSync(BUILD_DIR, { recursive: true })
+fs.mkdirSync(BUILD_DIR, { recursive: true })
 
 processImages(BUILD_DIR)
 processHTMLTemplates(BUILD_DIR, PAGES_DIR)
