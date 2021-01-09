b4bcbda765564b8e6b65a93e65c691eed7b25c96
Dana Ross <dana@danaross.dev>
2021-01-09 17:03:49 -0500

    Fixed XML

--- src/generate-sitemap.js
+++ src/generate-sitemap.js
@@ -4,13 +4,15 @@ const path = require("path");
 module.exports = (buildDir, urlRegistry) => {
   console.log(path.resolve(buildDir, "sitemap.xml"));
   const sitemapFile = fs.openSync(path.resolve(buildDir, "sitemap.xml"), "w");
+  fs.writeSync(sitemapFile, '<?xml version="1.0" encoding="UTF-8"?>\n');
   fs.writeSync(
     sitemapFile,
-    '<xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
   );
-  fs.writeSync(sitemapFile, '<?xml version="1.0" encoding="UTF-8"?>\n');
+
   urlRegistry.forEach((value, key) => {
     fs.writeSync(sitemapFile, `<url><loc>${value}</loc></url>\n`);
   });
+  fs.writeSync(sitemapFile, "</urlset>");
   fs.close(sitemapFile);
 };
